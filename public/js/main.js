4804d5b9468ff8f008b37571674930c6d201930a
rboure <remi.boure@etudiant.univ-lr.fr>
2022-08-09 13:40:07 +0200

    Change server websocket URL

--- public/js/main.js
+++ public/js/main.js
@@ -22,7 +22,7 @@ import { Playlist } from './playlist.js';
     };
 
     async function connectToServer() {
-        const ws = new WebSocket('ws://kacky-united-schedule.herokuapp.com:7071/ws');
+        const ws = new WebSocket('ws://tmuf-status-handler.herokuapp.com:7071/ws');
         return new Promise((resolve, reject) => {
             const timer = setInterval(() => {
                 if(ws.readyState === 1) {
