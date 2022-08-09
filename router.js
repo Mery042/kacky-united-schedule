ed5149c315551c581378acd250f9f945e83c1ecb
rboure <remi.boure@etudiant.univ-lr.fr>
2022-08-09 18:47:34 +0200

    Add Procfile

--- router.js
+++ router.js
@@ -8,6 +8,6 @@ app.get('*', (req, res) => {
   res.render('index.html')
 })
 
-app.listen(process.env.PORT || port, () => {
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
