4fe2507496ad88fa6f985a08cc7a7ef027d303a6
rboure <remi.boure@etudiant.univ-lr.fr>
2022-08-09 13:16:59 +0200

    fix port

--- router.js
+++ router.js
@@ -8,6 +8,6 @@ app.get('*', (req, res) => {
   res.render('index.html')
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening on port ${port}`)
 })
