3cfa81ddbe34bbecae3120f67ee6638b1f10f336
rboure <remi.boure@etudiant.univ-lr.fr>
2022-08-10 13:15:24 +0200

    change wss to ws

--- router.js
+++ router.js
@@ -6,8 +6,8 @@ const port = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, './public')));
 app.get('*', (req, res) => {
   res.render('index.html')
-})
+});
 
-app.listen(port, () => {
+app.listen(port, () => 
   console.log(`Example app listening on port ${port}`)
-})
+);
